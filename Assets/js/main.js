858417d67bcc85e5f4c8ed7f6412aff2658a360e
Murakumo <32967450+Murakumo-JP@users.noreply.github.com>
2024-10-27 14:43:21 +0300

    Pre-Update 7.1
    Небольшие изменения главной страницы, новый Header и правки в css.

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
 		$('.nav_floating_list').fadeToggle();
 	});
 	// Info Update
-	$('#inner_update').prepend('<p class="latest_update">Последнее обновление: 30.07.2024 | Патч: 7.05</p>');
+	$('#inner_update').prepend('<p class="latest_update">Последнее обновление: ??.??.2024 | Патч: 7.1</p>');
 	$('#jq_patch').prepend('Все описания основаны на активных умениях и бонусах, полученных на 100 уровне.<br />Более подробную информацию об изменениях в активных и пассивных умениях можно найти в примечаниях к <a target="_blank" href="https://eu.finalfantasyxiv.com/lodestone/topics/detail/da01b0d2d5434cd2ccfcc87f733df7a590f97c00/">патчноутам</a>.');
 	//$('.job_skil_list, .warn_info').prepend('<div class="error_info"><h5>Важная информация!</h5><p>Обновлены все старые классы кроме двух новых.</p></div>');
 	$('.SE').append('<p>All images on the site are the property of SQUARE ENIX© and are used under the <a href="https://support.na.square-enix.com/rule.php?id=5382&tag=authc">Materials Usage License</a></p>')
