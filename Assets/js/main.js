53849889c83bfa2b3d91ff4740c521b899efd843
Murakumo <32967450+Murakumo-JP@users.noreply.github.com>
2024-11-04 17:22:54 +0300

    Up Menu
    Обновлено меню, перенесено на js место подгрузки html файла с отдельным куском кода, должно ускорить загрузки страницы но это не точно, если будут ошибки в работе возможет откат к старой версий.

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,6 +1,5 @@
 // Doom Load
 $(document).ready(function () {
-	$('#jq_menu').load('Other/MenuJob.html');
 	$('#jq_menu_cg').load('Other/MenuCG.html');
 	// Open JobMenu
 	$('.nav_floating_icon').click(() => {
@@ -75,3 +74,99 @@ function backToTop() {
 }
 
 backToTop();
+
+// Debag href link
+// document.addEventListener("DOMContentLoaded", function() {
+// 	const links = document.querySelectorAll('a');
+// 	links.forEach(link => {
+// 		 link.addEventListener('click', function(event) {
+// 			  event.preventDefault();
+// 			  const url = this.getAttribute('href') + '.html';
+// 			  window.location.href = url;
+// 		 });
+// 	});
+// });
+
+// Menu JobGuide
+
+$(document).ready(function () {
+	const menuData = [
+		{
+			name: "Tank",
+			jobs: [
+				{ name: "Paladin", link: "PLD" },
+				{ name: "Warrior", link: "WAR" },
+				{ name: "Dark Knight", link: "DRK" },
+				{ name: "Gunbreaker", link: "GNB" },
+			],
+		},
+		{
+			name: "Healer",
+			jobs: [
+				{ name: "White Mage", link: "WHM" },
+				{ name: "Scholar", link: "SCH" },
+				{ name: "Astrologian", link: "AST" },
+				{ name: "Sage", link: "SGE" },
+			],
+		},
+		{
+			name: "Melee DPS",
+			jobs: [
+				{ name: "Monk", link: "MNK" },
+				{ name: "Dragoon", link: "DRG" },
+				{ name: "Ninja", link: "NIN" },
+				{ name: "Samurai", link: "SAM" },
+				{ name: "Reaper", link: "RPR" },
+				{ name: "Viper", link: "VPR" },
+			],
+		},
+		{
+			name: "Physical Ranged DPS",
+			jobs: [
+				{ name: "Bard", link: "BRD" },
+				{ name: "Machinist", link: "MCH" },
+				{ name: "Dancer", link: "DNC" },
+			],
+		},
+		{
+			name: "Magical Ranged DPS",
+			jobs: [
+				{ name: "Black Mage", link: "BLM" },
+				{ name: "Summoner", link: "SMN" },
+				{ name: "Red Mage", link: "RDM" },
+				{ name: "Pictomancer", link: "PCT" },
+				{ name: "Blue Mage", link: "BLU" },
+			],
+		},
+		{
+			name: "Ссылки",
+			jobs: [
+				{ name: "Главная", link: "/" },
+				{ name: "Job Guide", link: "../JobGuide" },
+				{ name: "Crafting & Gathering Guide", link: "../CraftingGatheringGuide" },
+				{ name: "Gold Saucer Guide", link: "../GoldSaucer", hidden: true },
+			],
+		},
+	];
+
+	const $menuList = $('<ul class="jobguide_menu_list"></ul>');
+
+	menuData.forEach(function (category) {
+		const $category = $('<li class="jobguide_menu_list"></li>');
+		$category.append(`<span class="job_name_menu">${category.name}</span>`);
+
+		const $subMenu = $('<ul class="jobguide_sub_menu"></ul>');
+		category.jobs.forEach(function (job) {
+			const $job = $("<li></li>");
+			if (job.hidden) {
+				$job.attr("hidden", "true");
+			}
+			$job.append(`<a href="${job.link}"><p>${job.name}</p></a>`);
+			$subMenu.append($job);
+		});
+		$category.append($subMenu);
+		$menuList.append($category);
+	});
+
+	$("#jq_menu").append($menuList);
+});
