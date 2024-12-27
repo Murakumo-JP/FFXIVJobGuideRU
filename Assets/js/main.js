567fc62821ae52707767df483326b78753578b56
Murakumo <32967450+Murakumo-JP@users.noreply.github.com>
2024-12-27 20:44:35 +0300

    Migration to jquery
    Перенос остатков кода js на jquery для целостности кода

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,20 +1,14 @@
 $(document).ready(() => {
-	const WarningEnabled = true;
+	const WarningEnabled = false;
 	const DebugEnabled = false;
 	// Info Update
 	const addUpdateInfo = (date, patchVersion, patchLink) => {
-		$("#inner_update").prepend(
-			`<p>Последнее обновление: ${date} | Патч: ${patchVersion}</p>`
-		);
+		$("#inner_update").prepend(`<p>Последнее обновление: ${date} | Патч: ${patchVersion}</p>`);
 		$("#patch_info").prepend(
 			`Все описания основаны на активных умениях и бонусах, полученных на 100 уровне.<br/>Более подробную информацию об изменениях в активных и пассивных умениях можно найти в примечаниях к <a target="_blank" href="${patchLink}">патчноутам</a>.`
 		);
 	};
-	addUpdateInfo(
-		"17.12.2024",
-		"7.15",
-		"https://eu.finalfantasyxiv.com/lodestone/topics/detail/78bd87d42626b597d2a3a5d7075b56c2604cd14b"
-	);
+	addUpdateInfo("17.12.2024", "7.15", "https://eu.finalfantasyxiv.com/lodestone/topics/detail/78bd87d42626b597d2a3a5d7075b56c2604cd14b");
 
 	$(".SE").append(
 		'<p>All images on the site are the property of SQUARE ENIX© and are used under the <a href="https://support.na.square-enix.com/rule.php?id=5382&tag=authc">Materials Usage License</a></p>'
@@ -65,7 +59,7 @@ $(document).ready(() => {
 				const $subMenu = $('<ul class="jobguide_sub_menu"></ul>');
 
 				category.jobs.forEach((job) => {
-					const $job = $(`<li${job.hidden ? ' hidden' : ''}></li>`);
+					const $job = $(`<li${job.hidden ? " hidden" : ""}></li>`);
 					$job.append(`<a href="${job.link}"><p>${job.name}</p></a>`);
 					$subMenu.append($job);
 				});
@@ -77,41 +71,42 @@ $(document).ready(() => {
 	}
 
 	// Sort Skills
-	const $tbody = $("tbody[data-sort='true']");
-	const $sortButton = $("#sortButton");
-	const $rows = $tbody.children("tr");
-	const $sortIcon = $("#sortIcon");
-	const $sortText = $("#sortText");
+	// const $tbody = $("tbody[data-sort='true']");
+	// const $sortButton = $("#sortButton");
+	// const $rows = $tbody.children("tr");
+	// const $sortIcon = $("#sortIcon");
+	// const $sortText = $("#sortText");
 
-	let sortState = 1;
+	// let sortState = 1;
 
-	if ($tbody.length) {
-		$sortButton.show().click(() => {
-			sortState = 1 - sortState;
-			sortRows();
-		});
-		sortRows();
-	} else {
-		$sortButton.hide();
-	}
+	// if ($tbody.length) {
+	// 	$sortButton.show().click(() => {
+	// 		sortState = 1 - sortState;
+	// 		sortRows();
+	// 	});
+	// 	sortRows();
+	// } else {
+	// 	$sortButton.hide();
+	// }
 
-	const updateSortIconAndText = () => {
-		const state = sortState === 0
-			? { icon: "sort-descending.svg", text: "Обновлённые умения" }
-			: { icon: "sort-ascending.svg", text: "Список по умолчанию" };
-		$sortIcon.attr("src", `../Assets/img/svg/${state.icon}`);
-		$sortText.text(state.text);
-	};
+	// const updateSortIconAndText = () => {
+	// 	const state =
+	// 		sortState === 0
+	// 			? { icon: "sort-descending.svg", text: "Обновлённые умения" }
+	// 			: { icon: "sort-ascending.svg", text: "Список по умолчанию" };
+	// 	$sortIcon.attr("src", `../Assets/img/svg/${state.icon}`);
+	// 	$sortText.text(state.text);
+	// };
 
-	const sortRows = () => {
-		$rows.sort((a, b) => {
-			return sortState === 0
-				? $(b).hasClass("skill_update") - $(a).hasClass("skill_update")
-				: ($(a).attr("db-skill") || "").localeCompare($(b).attr("db-skill") || "");
-		});
-		$tbody.empty().append($rows);
-		updateSortIconAndText();
-	};
+	// const sortRows = () => {
+	// 	$rows.sort((a, b) => {
+	// 		return sortState === 0
+	// 			? $(b).hasClass("skill_update") - $(a).hasClass("skill_update")
+	// 			: ($(a).attr("db-skill") || "").localeCompare($(b).attr("db-skill") || "");
+	// 	});
+	// 	$tbody.empty().append($rows);
+	// 	updateSortIconAndText();
+	// };
 
 	// Warning Info
 	if (WarningEnabled) {
@@ -122,7 +117,12 @@ $(document).ready(() => {
 		};
 
 		const getCookie = (name) => {
-			return document.cookie.split("; ").find((cookie) => cookie.startsWith(`${name}=`))?.split("=")[1] || null;
+			return (
+				document.cookie
+					.split("; ")
+					.find((cookie) => cookie.startsWith(`${name}=`))
+					?.split("=")[1] || null
+			);
 		};
 
 		const showErrorInfo = (info) => {
@@ -174,26 +174,22 @@ $(document).ready(() => {
 		}
 	};
 	initSearch();
-});
-
-// Preloader
-window.onload = () => {
-	setTimeout(() => {
-		const preloader = document.getElementById("page-preloader");
-		if (preloader && !preloader.classList.contains("done")) {
-			preloader.classList.add("done");
+	// Preloader
+	setTimeout(function () {
+		const $preloader = $("#page-preloader");
+		if ($preloader.length && !$preloader.hasClass("done")) {
+			$preloader.addClass("done");
 			$("body").css("overflow-y", "visible");
 		}
 		const id = window.location.hash;
 		if (id) activateTab(id.substring(1));
 	}, 500);
-};
-
-// Smooth Scrolling
-$("a[href*='#']").click(function (e) {
-	e.preventDefault();
-	const target = $($(this).attr("href"));
-	if (target.length) {
-		$("html, body").animate({ scrollTop: target.offset().top - 48 }, 500);
-	}
+	// Smooth Scrolling
+	$("a[href*='#']").click(function (e) {
+		e.preventDefault();
+		const target = $($(this).attr("href"));
+		if (target.length) {
+			$("html, body").animate({ scrollTop: target.offset().top - 48 }, 500);
+		}
+	});
 });
