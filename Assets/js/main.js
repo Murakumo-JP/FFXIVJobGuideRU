34dd602e727a6a59690dfaf439454436802fd0ba
Murakumo <32967450+Murakumo-JP@users.noreply.github.com>
2025-05-30 15:09:01 +0300

    Patch 7.25 (#26)
    * Patch 7.2
    ---------
    Co-authored-by: Morrigan

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,43 +1,63 @@
-$(document).ready(() => {
-	const WarningEnabled = false;
-	const DebugEnabled = false;
-	// Info Update
-	const addUpdateInfo = (date, patchVersion, patchLink) => {
-		$("#inner_update").prepend(`<p>Последнее обновление: ${date} | Патч: ${patchVersion}</p>`);
-		$("#patch_info").prepend(`Все описания основаны на активных умениях и бонусах, полученных на 100 уровне.<br/>Более подробную информацию об изменениях в активных и пассивных умениях можно найти в примечаниях к <a target="_blank" href="${patchLink}">патчноутам</a>.`);
-	};
-	addUpdateInfo("25.03.2025", "7.2", "https://eu.finalfantasyxiv.com/lodestone/topics/detail/e8dc09ebc782c9c57de6489532ed55804541e0c7");
+let CONFIG = {};
 
+async function loadConfig() {
+	try {
+		const res = await fetch("../DB/Config.json");
+		CONFIG = await res.json();
+		App();
+	} catch (e) {
+		console.error("Не удалось загрузить Config.json", e);
+	}
+}
+
+function App() {
+	const {update_info, menu_path, global_search_path, warning_enabled, debug_enabled, warning_text} = CONFIG;
+	$("#inner_update").prepend(`<p>Последнее обновление: ${update_info.date} | Патч: ${update_info.patch}</p>`);
+	$("#patch_info").prepend(`Все описания основаны на активных умениях и бонусах, полученных на 100 уровне.<br/>Подробнее: <a target="_blank" href="${update_info.url}">патчноуты</a>.`);
 	$(".SE").append('<p>All images on the site are the property of SQUARE ENIX© and are used under the <a href="https://support.na.square-enix.com/rule.php?id=5382&tag=authc">Materials Usage License</a></p>');
-
+	// Preloader
+	setTimeout(function () {
+		const $preloader = $("#page-preloader");
+		if ($preloader.length && !$preloader.hasClass("done")) {
+			$preloader.addClass("done");
+			$("body").css("overflow-y", "visible");
+		}
+		const id = window.location.hash;
+		if (id) activateTab(id.substring(1));
+	}, 500);
 	// Open JobMenu
 	$(".nav_floating_icon").click(() => {
 		$(".nav_floating_list").fadeToggle();
 	});
-	// Back to Top
-	const button = $(".nome_app_top");
-	$(window).on("scroll", () => {
-		$(window).scrollTop() >= 200 ? button.fadeIn() : button.fadeOut();
+	// Smooth Scrolling
+	$("a[href*='#']").click(function (e) {
+		e.preventDefault();
+		const target = $($(this).attr("href"));
+		if (target.length) {
+			$("html, body").animate({scrollTop: target.offset().top - 48}, 500);
+		}
 	});
-	button.on("click", (e) => {
+	// Back to Top
+	const buttonTop = $(".nome_app_top");
+	const buttonTheme = $("#themeToggle");
+
+	$(window).on("scroll", () => {
+		const scrollY = $(window).scrollTop();
+
+		if (scrollY >= 200) {
+			buttonTop.fadeIn();
+			buttonTheme.fadeIn();
+		} else {
+			buttonTop.fadeOut();
+			buttonTheme.fadeOut();
+		}
+	});
+
+	buttonTop.on("click", (e) => {
 		e.preventDefault();
 		$("html, body").animate({scrollTop: 0}, 1000);
 	});
 	// Tabs
-
-	// OLD CODE ===========================
-	// const activateTab = (id) => {
-	// 	$(`.js-tab-trigger[data-tab="${id}"]`).addClass("active");
-	// 	$(`.js-tab-trigger:not([data-tab="${id}"])`).removeClass("active");
-	// 	$(`.js-tab-content[data-tab="${id}"]`).addClass("active");
-	// 	$(`.js-tab-content:not([data-tab="${id}"])`).removeClass("active");
-	// };
-
-	// $(".js-tab-trigger").click(function (e) {
-	// 	e.preventDefault();
-	// 	const id = $(this).data("tab");
-	// 	activateTab(id);
-	// });
 	const activateTab = (id) => {
 		const $tabTriggers = $(".js-tab-trigger");
 		const $tabContents = $(".js-tab-content");
@@ -51,7 +71,6 @@ $(document).ready(() => {
 		});
 		history.replaceState(null, null, `#${id}`);
 	};
-
 	$(".js-tab-trigger").on("click", function (e) {
 		e.preventDefault();
 		const id = $(this).data("tab");
@@ -68,10 +87,20 @@ $(document).ready(() => {
 	const menuType = $menuContainer.data("menu-type");
 
 	if (menuType === "MenuDoWDoM" || menuType === "MenuDoHDoL") {
-		const menuPath = menuType === "MenuDoWDoM" ? "../DB/MenuDoWDoM.json" : "../DB/MenuDoHDoL.json";
-		$.getJSON(menuPath, (menuData) => {
+		$.getJSON(menu_path, (menuData) => {
+			let menuArray = menuData[menuType];
+
+			if (!menuArray) {
+				console.error(`Меню для типа "${menuType}" не найдено.`);
+				return;
+			}
+
+			if (menuData.Links && Array.isArray(menuData.Links)) {
+				menuArray = [...menuArray, ...menuData.Links];
+			}
+
 			const $menuList = $('<ul class="jobguide_menu_list"></ul>');
-			menuData.forEach((category) => {
+			menuArray.forEach((category) => {
 				const $category = $('<li class="jobguide_menu_list"></li>');
 				$category.append(`<span class="job_name_menu">${category.name}</span>`);
 				const $subMenu = $('<ul class="jobguide_sub_menu"></ul>');
@@ -85,50 +114,7 @@ $(document).ready(() => {
 				$menuList.append($category);
 			});
 			$menuContainer.append($menuList);
-		}).fail(() => console.error("Данные пропили Муглы, все вопросы к ним."));
-	}
-	// Warning Info
-	if (WarningEnabled) {
-		const setCookie = (name, value, days) => {
-			const date = new Date();
-			date.setTime(date.getTime() + days * 86400000);
-			document.cookie = `${name}=${value};expires=${date.toUTCString()};path=/`;
-		};
-		const getCookie = (name) => {
-			return (
-				document.cookie
-					.split("; ")
-					.find((cookie) => cookie.startsWith(`${name}=`))
-					?.split("=")[1] || null
-			);
-		};
-		const showErrorInfo = (info) => {
-			$(".job_skil_list, .warn_info").prepend(`<div class="error_info" id="warnInfo"><h5>Важная информация!</h5><span id="closeInfo">✖</span><p>${info}</p></div>`);
-			if (getCookie("warnInfoHidden") === "true") {
-				$("#warnInfo").addClass("hidden");
-			}
-			$("#closeInfo").click(() => {
-				$("#warnInfo").addClass("hidden");
-				setCookie("warnInfoHidden", "true", 7);
-			});
-		};
-		showErrorInfo("Error");
-	}
-	// Debug
-	if (DebugEnabled) {
-		document.addEventListener("DOMContentLoaded", () => {
-			$("a").click(function (e) {
-				e.preventDefault();
-				window.location.href = `${$(this).attr("href")}.html`;
-			});
-		});
-		$("tr").each(function () {
-			const titleText = ["db-skill", "db-role-action", "db-skill-passive", "db-role-traits", "db-skill-pvp"]
-				.map((attr) => $(this).attr(attr))
-				.filter(Boolean)
-				.join(", ");
-			if (titleText) $(this).attr("title", titleText);
-		});
+		}).fail(() => console.error("Ошибка Menu.json не был загружен."));
 	}
 	// Search
 	const initSearch = () => {
@@ -145,29 +131,10 @@ $(document).ready(() => {
 		}
 	};
 	initSearch();
-	// Preloader
-	setTimeout(function () {
-		const $preloader = $("#page-preloader");
-		if ($preloader.length && !$preloader.hasClass("done")) {
-			$preloader.addClass("done");
-			$("body").css("overflow-y", "visible");
-		}
-		const id = window.location.hash;
-		if (id) activateTab(id.substring(1));
-	}, 500);
-	// Smooth Scrolling
-	$("a[href*='#']").click(function (e) {
-		e.preventDefault();
-		const target = $($(this).attr("href"));
-		if (target.length) {
-			$("html, body").animate({scrollTop: target.offset().top - 48}, 500);
-		}
-	});
-});
-// Global Search
-$(document).ready(function () {
+	// Global Search
 	let jsonData = [];
-	$.getJSON("../DB/GlobalSearch.json").done(function (data) {
+
+	$.getJSON(global_search_path).done(function (data) {
 		jsonData = data;
 	});
 
@@ -187,6 +154,7 @@ $(document).ready(function () {
 
 			if (query && jsonData.length) {
 				let hasResults = false;
+
 				jsonData.forEach((job) => {
 					const safeJobName = job.job.replace(/\s+/g, "_").toLowerCase();
 					job.skills.forEach((skill) => {
@@ -239,12 +207,88 @@ $(document).ready(function () {
 	if (encodedSkill) {
 		try {
 			let scrollToSkill = atob(encodedSkill);
-			let target = $(`[db-skill='${scrollToSkill}']`);
-			if (target.length) {
-				$("html, body").animate({scrollTop: target.offset().top - 48}, 500);
-			}
+			let attempts = 0;
+			let maxAttempts = 20;
+			let interval = setInterval(() => {
+				let target = $(`[db-skill='${scrollToSkill}']`);
+				if (target.length) {
+					$("html, body").animate({scrollTop: target.offset().top - 48}, 500);
+					clearInterval(interval);
+				} else if (++attempts >= maxAttempts) {
+					clearInterval(interval);
+					console.warn("Элемент не найден для прокрутки:", scrollToSkill);
+				}
+			}, 100);
 		} catch (error) {
-			console.error("Ошибка декодирования skill:", error);
+			console.error("Ошибка декодирования параметра skill:", error);
 		}
 	}
+	// Warning Info
+	if (warning_enabled) {
+		const setCookie = (name, value, days) => {
+			const date = new Date();
+			date.setTime(date.getTime() + days * 86400000);
+			document.cookie = `${name}=${value};expires=${date.toUTCString()};path=/`;
+		};
+		const getCookie = (name) => {
+			return (
+				document.cookie
+					.split("; ")
+					.find((cookie) => cookie.startsWith(`${name}=`))
+					?.split("=")[1] || null
+			);
+		};
+		const showErrorInfo = (info) => {
+			$(".job_skil_list, .warn_info").prepend(`<div class="error_info" id="warnInfo"><h5>Важная информация!</h5><span id="closeInfo">✖</span><p>${info}</p></div>`);
+			if (getCookie("warnInfoHidden") === "true") {
+				$("#warnInfo").addClass("hidden");
+			}
+			$("#closeInfo").click(() => {
+				$("#warnInfo").addClass("hidden");
+				setCookie("warnInfoHidden", "true", 7);
+			});
+		};
+		showErrorInfo(warning_text);
+	}
+	// Debug
+	if (debug_enabled) {
+		document.addEventListener("DOMContentLoaded", () => {
+			$("a").click(function (e) {
+				e.preventDefault();
+				window.location.href = `${$(this).attr("href")}.html`;
+			});
+		});
+		$("tr").each(function () {
+			const titleText = ["db-skill", "db-role-action", "db-skill-passive", "db-role-traits", "db-skill-pvp"]
+				.map((attr) => $(this).attr(attr))
+				.filter(Boolean)
+				.join(", ");
+			if (titleText) $(this).attr("title", titleText);
+		});
+	}
+}
+
+$(document).ready(loadConfig);
+
+// Light/Dark Theme
+$(document).ready(() => {
+	function applyTheme(theme) {
+		document.body.classList.remove("light-theme", "dark-theme");
+		document.body.classList.add(`${theme}-theme`);
+
+		const iconPath = theme === "dark" ? "../Assets/img/svg/sun.svg" : "../Assets/img/svg/moon.svg";
+		$("#themeToggle .icon").attr("src", iconPath);
+	}
+
+	function toggleTheme() {
+		const isDark = $("body").hasClass("dark-theme");
+		const newTheme = isDark ? "light" : "dark";
+		applyTheme(newTheme);
+		localStorage.setItem("theme", newTheme);
+	}
+
+	const savedTheme = localStorage.getItem("theme") || "light";
+	applyTheme(savedTheme);
+
+	$("#themeToggle").on("click", toggleTheme);
 });
